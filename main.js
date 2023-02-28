9ade07b6b7412dfca24ea680c564bfcfbbacd328
Enzo Farias <enzofarias030603@gmail.com>
2023-02-27 23:05:39 -0300

    commit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import imagesLoaded from "imagesloaded";
 
 const content = document.querySelector("section");
@@ -7,25 +6,21 @@ const content = document.querySelector("section");
 const imgLoad = imagesLoaded(content);
 
 imgLoad.on("done", instance => {
-  
-
-  gsap.to("svg", {
-    scale:0
-  })
-
-  gsap.to(".blinder", {
-    scaleY: 0,
-    stagger: .3,
-    ease: "power3.out"
-  })
 
   const tl = gsap.timeline();
 
-  tl.to(["h1", "p"], {
-    y: 0,
-    stagger: .3,
-    duration: 2,
-    ease: "power4.out"
+  tl.to("svg", {
+    scale: 0
   })
+    .to(".blinder", {
+      scaleY: 0,
+      stagger: .3,
+      ease: "power3.out"
+    }).to(["h1", "p"], {
+      y: 0,
+      stagger: .3,
+      duration: 2,
+      ease: "power4.out"
+    })
 
 })
